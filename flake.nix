2caba2f570c7bfee75e84439c56f12f8b38796a0
Manuel Bovo <manuel.bovo@gmail.com>
2025-02-03 17:40:25 +0100

    Update config for new host

--- flake.nix
+++ flake.nix
@@ -78,6 +78,25 @@
         # ==================================================================================================================
 
         # ==================================================================================================================
+        # new work MacOs configuration
+        # ==================================================================================================================
+        "manuelbovo@M-PA-LT75QJ7NN7" = home-manager.lib.homeManagerConfiguration rec{
+          pkgs = extraSpecialArgs.pkgs-stable; # using nixpgs-stable as default (see below)
+          extraSpecialArgs = rec{
+            system = "aarch64-darwin";
+            hostname = "M-PA-LT75QJ7NN7";
+            username = "manuelbovo";
+            homeDirectory = "${paths.home.${system}}${username}";
+            pkgs-stable = inputs.nixpkgs-stable-darwin.legacyPackages.${system};
+            pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+            pdh = inputs.pdhpkg.packages.${system};
+            priv-config = inputs.nix-configs-priv;
+          };
+          modules = mods.common ++ mods.darwin ++ [ "${extraSpecialArgs.priv-config}/hosts/${extraSpecialArgs.hostname}/nix/custom.nix" ];
+        };
+        # ==================================================================================================================
+
+        # ==================================================================================================================
         # work MacOs configuration
         # ==================================================================================================================
         "manuel.bovo@manuel.bovo" = home-manager.lib.homeManagerConfiguration rec{
