9ef051c7623bc9db5fca2dfdef2f6c8c964e704f
Manuel Bovo <manuel.bovo@gmail.com>
2025-07-25 15:32:29 +0200

    Update nixify

--- config/flakes/venv.nix
+++ config/flakes/venv.nix
@@ -1,7 +1,7 @@
 {
   description = "generic venv flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
